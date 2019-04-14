bce8e33ef0991225cf31d65203b5e9eeb45cec5c
janivanecky <jan.ivanecky@hotmail.com>
2019-04-14 13:21:41 -0700

    Cleanup 3D particle shader a bit

--- shaders/particle_shader_3d.hlsl
+++ shaders/particle_shader_3d.hlsl
@@ -50,40 +50,33 @@ void main(uint index : SV_GroupIndex, uint3 group_id :SV_GroupID){
     uint idx = index + 1000 * (group_id.x + group_id.y * 10 + group_id.z * 100);
     float halfpi = 3.1415 / 2.0f;
 
+    // Fetch current particle state
     float x = particles_x[idx];
     float y = particles_y[idx];
     float z = particles_z[idx];
-    
     float t = particles_theta[idx];
     float ph = particles_phi[idx];
 
-    float x_center = (world_width  / 2.0 - x);
-    float y_center = (world_height / 2.0 - y);
-    float z_center = (world_depth / 2.0 - z);
-    float3 d_center_v = float3(x_center, y_center, z_center);
-    float d_center = length(d_center_v);
-    float r = float(wang_hash(idx) % 1000.0) / 1000.0 * 0.5 + 0.5f;
+    // Get vector which points in the current particle's direction 
+    float3 center_axis = float3(sin(t) * cos(ph), cos(t), sin(t) * sin(ph));
+    
+    // Get base vector which points away from the current particle's direction and will be used
+    // to sample environment in other directions
+    float sense_theta = t - sense_spread;
+    float3 off_center_base_dir = float3(sin(sense_theta) * cos(ph), cos(sense_theta), sin(sense_theta) * sin(ph));
+
+    // Sample environment straight ahead
     int3 p = int3(x, y, z);
+    float3 center_sense_pos = center_axis * sense_distance;
 
-    //float d_c = //sin(d_center / 20.0) * 0.5f + 0.5f;
-    float d_c = 1.0;//1.0 - clamp((d_center - 50.0) / 150.0, 0, 1.0);
-    float d_c_turn = clamp((d_center - 50.0) / 150.0, 0, 1) * center_attraction;
-    float tc = t;
-    float pc = ph;
-    float dt = t - sense_spread;
-    float3 dc = float3(sin(tc) * cos(pc), cos(tc), sin(tc) * sin(pc)) * sense_distance * d_c * r;
-    float3 axis = normalize(dc);
-    float3 dd = float3(sin(dt) * cos(pc), cos(dt), sin(dt) * sin(pc));// * sense_distance * d_c * r;
-    float start_angle = random(idx * 42) * 3.1415 - halfpi;
-    float stuffs[5];
-
-    // Value straight ahead
-    float max_value = tex_in[int3(dc) + p];
+    // Sample environment away from the center axis and store max values.
+    float max_value = tex_in[int3(center_sense_pos) + p];
     int max_value_count = 1;
     int max_values[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    float start_angle = random(idx * 42) * 3.1415 - halfpi;
     for (int i = 1; i < 9; ++i) {
         float angle = start_angle + halfpi / 2.0 * i;
-        float3 sense_position = rotate(dd, axis, angle) * sense_distance * d_c * r;
+        float3 sense_position = rotate(off_center_base_dir, center_axis, angle) * sense_distance;
         float stuff = tex_in[int3(sense_position) + p];
         if (stuff > max_value) {
             max_value_count = 1;
@@ -94,18 +87,23 @@ void main(uint index : SV_GroupIndex, uint3 group_id :SV_GroupID){
         }
     }
 
-    uint r_direction = wang_hash(idx * uint(x) * uint(y) * uint(z)) % max_value_count;
-    int direction = max_values[r_direction];
-    float tla = t - turn_angle;
-    float3 td = float3(sin(tla) * cos(pc), cos(tla), sin(tla) * sin(pc));
+    // Pick direction with max value sampled.
+    uint random_max_value_direction = wang_hash(idx * uint(x) * uint(y) * uint(z)) % max_value_count;
+    int direction = max_values[random_max_value_direction];
+    float theta_turn = t - turn_angle;
+    float3 off_center_base_dir_turn = float3(sin(theta_turn) * cos(ph), cos(theta_turn), sin(theta_turn) * sin(ph));
     if (direction > 0) {
-        float3 best_direction = rotate(td, axis, direction * halfpi / 2.0 + start_angle);
+        float3 best_direction = rotate(off_center_base_dir_turn, center_axis, direction * halfpi / 2.0 + start_angle);
         ph = atan2(best_direction.z, best_direction.x);
         t = acos(best_direction.y / length(best_direction));
     }
 
+    // Compute rotation applied by force pointing to the center of environment.
+    float3 to_center = float3(world_width  / 2.0 - x, world_height / 2.0 - y, world_depth / 2.0 - z);
+    float d_center = length(to_center);
+    float d_c_turn = clamp((d_center - 50.0) / 150.0, 0, 1) * center_attraction;
     float3 dir = float3(sin(t) * cos(ph), cos(t), sin(t) * sin(ph));
-    float3 center_dir = normalize(d_center_v);
+    float3 center_dir = normalize(to_center);
     float3 center_angle = acos(dot(dir, center_dir));
     float st = 0.1 * d_c_turn;
     dir = sin((1 - st) * center_angle) / sin(center_angle) * dir + sin(st * center_angle) / sin(center_angle) * center_dir;
@@ -114,15 +112,18 @@ void main(uint index : SV_GroupIndex, uint3 group_id :SV_GroupID){
         ph = atan2(dir.z, dir.x);
     }
 
-    float3 dp = float3(sin(t) * cos(ph), cos(t), sin(t) * sin(ph)) * move_distance;// * r;
+    // Make a step
+    float3 dp = float3(sin(t) * cos(ph), cos(t), sin(t) * sin(ph)) * move_distance;
     x += dp.x;
     y += dp.y;
     z += dp.z;
 
+    // Keep the particle inside environment
     x = mod(x, world_width);
     y = mod(y, world_height);
     z = mod(z, world_depth);
 
+    // Check for collisions
     uint val = 0;
     InterlockedCompareExchange(tex_occ[uint3(x, y, z)], 0, uint(collision), val);
     if (val == 1.0) {
@@ -133,6 +134,7 @@ void main(uint index : SV_GroupIndex, uint3 group_id :SV_GroupID){
         ph = float(wang_hash(idx * uint(x) * uint(y) * uint(z) + 12) % 1000) / 1000.0 * 3.1415 * 2.0;
     }
 
+    // Update particle state
     particles_x[idx] = x;
     particles_y[idx] = y;
     particles_z[idx] = z;
